8efd63763a5a27e2eaaa76eb485ab90f10be6cc1
tarekyehya <tarek_500@yahoo.com>
2024-10-03 15:13:46 +0300

    add mongo auth

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     class Config:
         env_file = ".env"
